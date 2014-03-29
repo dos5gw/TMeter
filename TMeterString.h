b2d035f6b0d5b096193e9fa3f9f491543ea38c67
dos5gw <chen@fedora.org>
2014-03-29 15:04:58 +0800

    delete useless and binary

--- TMeterString.h
+++
@@ -1,7 +0,0 @@
-#ifndef _TMETER_STRING_H_
-#define _TMETER_STRING_H_
-
-BOOL CheckDelimiterNum(CString buff,int num,CString dlt);
-CString GetStringAt(CString buff,int num, CString dlt);
-
-#endif
--- TMeterTypeDefine.h
+++
@@ -1,6 +0,0 @@
-#ifndef TM_TYPE_DEFINE_H
-#define TM_TYPE_DEFINE_H
-
-#define SPEC_SIZE 256
-
-#endif
